e82fa29dd226986f0018b8751068b04237992387
Austin Vasquez <joevatin@gmail.com>
2023-09-17 14:27:08 -0400

    Step 8

---
+++ lab-tunley/AlbumCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCell.swift
+//  lab-tunley
+//
+//  Created by Austin Vasquez on 9/17/23.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    @IBOutlet weak var albumImageView: UIView!
+    
+}
