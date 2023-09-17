f5d98ed97c53401ff2c92457f2cb8cd534f30ce3
Joevatin <136493001+Joevatin@users.noreply.github.com>
2023-09-17 18:24:14 -0400

    Add files via upload

---
+++ lab-tunley/TracksViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController.swift
+//  lab-tunley
+//
+//  Created by Charlie Hieger on 12/1/22.
+//
+//
+import UIKit
+
+class TracksViewController: UIViewController, UITableViewDataSource {
+    
+    // TODO: Pt 1 - Add table view outlet
+    @IBOutlet weak var tableView: UITableView!
+    
+    // TODO: Pt 1 - Add a tracks property
+    var tracks: [Track] = []
+    
+    override func viewDidLoad() {
+        // need protocal UITableViewDataSource
+        tableView.dataSource = self
+        super.viewDidLoad()
+        
+        // TODO: Pt 1 - Set tracks property
+        // Create a URL for the request
+        // In this case, the custom search URL you created in in part 1
+        let url = URL(string: "https://itunes.apple.com/search?term=blackpink&attribute=artistTerm&entity=song&media=music")!
+        
+        // Use the URL to instantiate a request
+        let request = URLRequest(url: url)
+        
+        // Create a URLSession using a shared instance and call its dataTask method
+        // The data task method attempts to retrieve the contents of a URL based on the specified URL.
+        // When finished, it calls it's completion handler (closure) passing in optional values for data (the data we want to fetch), response (info about the response like status code) and error (if the request was unsuccessful)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            
+            // Handle any errors
+            if let error = error {
+                print("❌ Network error: \(error.localizedDescription)")
+            }
+            
+            // Make sure we have data
+            guard let data = data else {
+                print("❌ Data is nil")
+                return
+            }
+            // The `JSONSerialization.jsonObject(with: data)` method is a "throwing" function (meaning it can throw an error) so we wrap it in a `do` `catch`
+            // We cast the resultant returned object to a dictionary with a `String` key, `Any` value pair.
+            do {
+                // Create a JSON Decoder
+                let decoder = JSONDecoder()
+                // Create a date formatter
+                let dateFormatter = DateFormatter()
+
+                // Set a custom date format based on what we see coming back in the JSON
+                dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+
+                // Set the decoding strategy on the JSON decoder to use our custom date format
+                decoder.dateDecodingStrategy = .formatted(dateFormatter)
+                
+                // Use the JSON decoder to try and map the data to our custom model.
+                // TrackResponse.self is a reference to the type itself, tells the decoder what to map to.
+                let response = try decoder.decode(TracksResponse.self, from: data)
+                // Access the array of tracks from the `results` property
+                let tracks = response.results
+                
+                DispatchQueue.main.async {
+                    self?.tracks = tracks
+                    self?.tableView.reloadData()
+                }
+                print("✅ \(tracks)")
+            } catch {
+                print("❌ Error parsing JSON: \(error.localizedDescription)")
+            }
+            
+        }
+        
+        // Initiate the network request
+        task.resume()
+        print(tracks)
+        print("👋 Below the closure")   // asynchronously
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // TODO: Pt 1 - Pass the selected track to the detail view controller
+        // Get the cell that triggered the segue
+        if let cell = sender as? UITableViewCell,
+           // Get the index path of the cell from the table view
+           let indexPath = tableView.indexPath(for: cell),
+           // Get the detail view controller
+           let detailViewController = segue.destination as? DetailViewController {
+            
+            // Use the index path to get the associated track
+            let track = tracks[indexPath.row]
+            
+            // Set the track on the detail view controller
+            detailViewController.track = track
+        }
+        
+    }
+    
+    // TODO: Pt 1 - Add table view data source methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackCell", for: indexPath) as! TrackCell
+        let track = tracks[indexPath.row]
+        cell.configure(with: track)
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let indexPath = tableView.indexPathForSelectedRow{
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+    
+}
